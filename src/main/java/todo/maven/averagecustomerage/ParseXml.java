53a001906285db5a1a69caf93573f2d7fb13e58a
kazuhirokomoda <kazuhirokomoda@gmail.com>
2015-07-11 22:07:04 +0900

    initial commit

---
+++ src/main/java/todo/maven/averagecustomerage/ParseXml.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package todo.maven.averagecustomerage;
+
+/**
+ *
+ * @author kkomoda
+ */
+public class ParseXml {
+    
+}
